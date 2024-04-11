fd8c91a7c2e272074270d020a8a8efa44e0c5043
Luis Cordova <lcordovcarrion@gmail.com>
2024-04-11 17:06:50 -0500

    Taller 0

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -5,6 +5,7 @@
  */
 package paquete2;
 
+import java.util.Scanner;
 import paquete1.Mensaje;
 import paquete3.Informacion;
 
@@ -12,20 +13,27 @@ import paquete3.Informacion;
  *
  * @author reroes
  */
-public class Demo {
+public class Demo002 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         String cadena1 = Mensaje.obtenerMensaje();
-        /* 
-        Llamamos al metodo obtenerMensaje mediante el nombre de la clase y 
-        luego el nombre de la funcion para obtener la cadena
-        */
+        String cadenaFinal;
         int habitantes = Informacion.obtenerHabitantes();
+        int opcion;
         // usar printf;
-        System.out.printf("%s con %d\n", cadena1, habitantes);
+        cadenaFinal = String.format("%s con %d\n", cadena1, habitantes);
+        System.out.println("Ingrese un numero para imprimir la cadena");
+        opcion = entrada.nextInt();
+        if (opcion >= 1 && opcion <= 5) {
+            System.out.printf("%s", cadenaFinal);
+        } else {
+            System.out.printf("%s", cadenaFinal.toUpperCase());
+        }
+
     }
 
 }
